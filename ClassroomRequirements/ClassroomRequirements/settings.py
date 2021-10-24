86d7a7384faac8b20564bcc20f10164295e40d88
Rayhan Farhan <robindriscol@gmail.com>
2021-10-24 22:30:45 +0600

    updated base setup

--- ClassroomRequirements/ClassroomRequirements/settings.py
+++ ClassroomRequirements/ClassroomRequirements/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'ClassroomRequirements.wsgi.application'
 
 DB_NAME = "classroom_requirements"
 DB_USER = "postgres"
-DB_PASSWORD = "96320"
+DB_PASSWORD = ""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
